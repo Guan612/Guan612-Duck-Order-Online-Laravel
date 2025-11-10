625efd5564d572c1b16e3db2e76a12c78fb0023d
Guan612 <tiantony53@163.com>
2025-11-10 20:33:24 +0800

    初步配置完成

--- resources/js/pages/Home.tsx
+++ resources/js/pages/Home.tsx
@@ -1,9 +1,12 @@
+import { Button } from 'antd';
+
 export default function Home() {
     return (
-        <div style={{ padding: '2rem', textAlign: 'center' }}>
-            <h1>欢迎来到我的新前端！</h1>
+        <div className="flex flex-col justify-center p-2">
+            <h1 className="font-bold">欢迎来到我的新前端！</h1>
             <p>这是一个从零开始的 React 页面。</p>
             <p>你现在可以开始构建你的在线点餐系统了。</p>
+            <Button type="primary">测试按钮</Button>
         </div>
     );
 }
